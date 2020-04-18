6b20c0e294868cbe6e22b75e2445ad9a8ba08201
nicholasykng <nicholasykng@gmail.com>
2020-04-17 20:59:29 -0700

    troubleshooting routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import './App.css';
 import Home from './components/Home'
 import Parks from './containers/Parks'
+import ParkShow from './containers/ParkShow'
 //import ParkNew from './containers/ParkNew'<Route exact path="/new" component={ParkNew} />
 
 function App() {
@@ -12,8 +13,8 @@ function App() {
       <div className="App">
       <NavBar />
       <Route exact path="/" component={Home} />
-      <Route path='/parks' render={routerProps => <Parks {...routerProps} />} />
-      
+      <Route path="/parks" render={routerProps => <Parks {...routerProps} />} />
+      <Route path="/parks/:parkId" component={ParkShow} />
       </div>
     </Router>
   );
--- client/src/containers/ParkShow.js
+++ client/src/containers/ParkShow.js
@@ -5,7 +5,11 @@ class ParkShow extends Component {
         const {parks, match} = this.props
         return (
             <div>
-                <h2>{parks[match.params.parkId].name}</h2>
+                <h2>{parks[match.params.parkId + 1].name}</h2>
+                <p>{parks[match.params.parkId + 1].description}</p>
+                <p>{parks[match.params.parkId + 1].activities}</p>
+                <p>{parks[match.params.parkId + 1].directions}</p>
+                <img src={parks[match.params.parkId + 1].images} width="100%" height="100%" alt="pic"/>
             </div>
         )
     }
