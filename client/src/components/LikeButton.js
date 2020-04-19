365f7f38c6b66729f101fdf48641e5d4f9548c82
nicholasykng <nicholasykng@gmail.com>
2020-04-19 09:55:42 -0700

    troubleshooting like button not iterable

---
+++ client/src/components/LikeButton.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import {Button, Badge} from 'react-bootstrap'
+
+class LikeButton extends Component {
+    render() {
+        return (
+            <div>
+                <Button className="like-park" onClick={this.props.likePark}>
+                Like<Badge variant="light">{this.props.park.likes}</Badge>
+                </Button>
+            </div>
+        )
+    }
+}
+export default LikeButton
