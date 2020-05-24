8ededa4910ef6f6446ed03d888593742d85321d8
nicholasykng <nicholasykng@gmail.com>
2020-05-23 20:58:57 -0700

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_04_17_031252) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.string "commenter"
